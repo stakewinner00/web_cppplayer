820cb6a7e2a7423b651e3bf20f1099b65b21ed0c
Brandon Sanchez <bssanchez93@gmail.com>
2016-06-14 12:14:57 -0500

    [*] Finalizados play, pause, stop, cambio de volumen, falta en prev y next tomar los datos ojalá en JSON y setearlos en el título y la imagen... funcionalidad de prev y next ya está salvo lo mencionado

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,11 +1,76 @@
 $(document).ready(function() {
-    $('#btn-play-pause').click(function() {
-        if ($($('#btn-play-pause').find('.material-icons')).text() == 'pause') {
-            // Envía ajax de Pausar /pause
-            $($('#btn-play-pause').find('.material-icons')).text('play_arrow')
-        } else {
-            // Envía ajax de Play /play
-            $($('#btn-play-pause').find('.material-icons')).text('pause')
-        }
-    });    
+	$('#btn-play-pause').click(function(e) {
+		e.preventDefault();
+
+		if ($($('#btn-play-pause').find('.material-icons')).text() == 'pause') {
+			$.ajax({
+				url: '/pause',
+				method: 'GET',
+				async: true,
+				dataType: 'text',
+				success: function(data) {
+					$($('#btn-play-pause').find('.material-icons')).text('play_arrow');
+				}
+			});
+		} else {
+			$.ajax({
+				url: '/play',
+				method: 'GET',
+				async: true,
+				dataType: 'text',
+				success: function(data) {
+					$($('#btn-play-pause').find('.material-icons')).text('pause');
+				}
+			});            
+		}
+	});
+
+	$('#btn-next').click(function(e) {
+		e.preventDefault();
+		$.ajax({
+			url: '/next',
+			method: 'GET',
+			async: true,
+			dataType: 'text',
+			success: function(data) {
+				// Función para obtener datos de la canción y setearlos en la página
+			}
+		});
+	});
+
+	$('#btn-prev').click(function(e) {
+		e.preventDefault();
+		$.ajax({
+			url: '/prev',
+			method: 'GET',
+			async: true,
+			dataType: 'text',
+			success: function(data) {
+				// Función para obtener datos de la canción y setearlos en la página
+			}
+		});
+	});
+
+	$('#btn-stop').click(function(e) {
+		e.preventDefault();
+		$.ajax({
+			url: '/stop',
+			method: 'GET',
+			async: true,
+			dataType: 'text',
+			success: function(data) {}
+		});
+	});
+
+	$('#valor-volumen').change(function() {
+		var vol = $(this).val();
+
+		$.ajax({
+			url: '/volumen/' + vol,
+			method: 'GET',
+			async: false,
+			dataType: 'text',
+			success: function(data) {}
+		});
+	});
 });
