35e9f2966d7c3e4aa6507ea3a97656e835ce925a
Brandon Sanchez <bssanchez93@gmail.com>
2016-06-17 13:46:08 -0500

    [*] Tema dark y sistemas de volumen agregados

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,3 +1,47 @@
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        }
+    return null;
+}
+
+function ChangeTheme(theme) {
+    var d = new Date();
+    d.setTime(d.getTime() + (365*24*60*60*1000));
+
+    document.cookie = "tema=" + theme + "; expires=" + d.toUTCString();
+
+    if (theme === 'dark') {
+        $('.blue')
+            .removeClass('blue')
+            .addClass('teal');
+
+        $('body')
+            .addClass('blue-grey')
+            .addClass('darken-4');
+
+        $('.card')
+            .addClass('blue-grey')
+            .addClass('lighten-1');
+    } else {
+        $('.teal')
+            .removeClass('teal')
+            .addClass('blue');
+
+        $('body')
+            .removeClass('blue-grey')
+            .removeClass('darken-4');
+
+        $('.card')
+            .removeClass('blue-grey')
+            .removeClass('lighten-1');
+    }
+}
+
 $(document).ready(function() {
 	$('#btn-play-pause').click(function(e) {
 		e.preventDefault();
@@ -66,11 +110,43 @@ $(document).ready(function() {
 		var vol = $(this).val();
 
 		$.ajax({
-			url: '/volumen/' + vol,
+			url: '/setvolumen/' + vol,
 			method: 'GET',
 			async: false,
 			dataType: 'text',
 			success: function(data) {}
 		});
 	});
+
+    $(".button-collapse").sideNav();
+    
+    
+    /** Datos de precarga **/
+    var tema = 'light';
+    if (getCookie('tema') != null) {
+        tema = getCookie('tema');
+    }
+
+    ChangeTheme(tema);
+
+    $('.change-theme').click(function(e){
+        e.preventDefault();
+        ChangeTheme($(this).attr('data-theme'));
+    });
+
+    // Prevcarga Volumen
+    $.ajax({
+        url: '/setvolumen/' + vol,
+        method: 'GET',
+        async: false,
+        dataType: 'text',
+        success: function(data) {
+            var vol = 100;
+            if (data != null) {
+                vol = data;
+            }
+            
+            $('#valor-volumen').val(vol);
+        }
+    });
 });
